81fb579c5bc68e4b12e8742a3b4760536392a52a
Matthias Felleisen <matthias@ccs.neu.edu>
2023-02-17 20:46:25 -0500

    rec scope

--- Lib/environments.rkt
+++ Lib/environments.rkt
@@ -1,8 +1,8 @@
 #lang typed/racket
 
-(provide Env new-env env-add env-retrieve)
+(provide Env new-env env-add env-rec env-retrieve)
 
-(define-type [Env α] (Listof [List Symbol α]))
+(define-type [Env α] (Listof (U [List Symbol 'plain α] [List Symbol 'rec α [Env α]])))
 
 (: new-env (All (α) (-> [Env α])))
 (define (new-env)
@@ -10,10 +10,16 @@
   
 (: env-add (All (α) (-> [Env α] Symbol α [Env α])))
 (define (env-add env x m)
-  (cons [list x m] env))
+  (cons [list x 'plain m] env))
 
-(: env-retrieve (All (α) (-> [Env α] Symbol String α)))
-(define (env-retrieve env x fmt)
+(: env-rec (All (α) (-> [Env α] Symbol α [Env α])))
+(define (env-rec env x m)
+  (cons [list x 'rec m env] env))
+
+(: env-retrieve (All (α) (->* [[Env α] Symbol String] ([-> Symbol α [Env α] α]) α)))
+(define (env-retrieve env x fmt (f (λ (s a e) (error 'value fmt x))))
   (define v (assq x env))
-  (unless v (error 'value fmt x))
-  (second v))
+  (match v
+    [#false              (error 'value fmt x)]
+    [(list x 'plain m)   m]
+    [(list x 'rec m env) (f x m env)]))
